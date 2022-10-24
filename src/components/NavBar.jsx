edc0c345d2fd503d7480695528de14645162c23a
laureanomarenco <laureanomarenco@gmail.com>
2022-10-24 09:37:47 -0300

    fix-responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,7 @@
 import "../App.css";
 import data from "../data.json";
 import React from "react";
-import changeLenguage from "../store/actions";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import n from './NavBar.module.css'
 
 export default function NavBar() {
@@ -16,9 +15,9 @@ export default function NavBar() {
           <img src='https://res.cloudinary.com/dg7ssgadn/image/upload/v1666547390/L_pgrvno.png' href='#land' className={n.logoLink}></img>
         </a>
         <div>
-        <a className={n.navLink} href='#info'>Info</a>
-        <a className={n.navLink} href='#projects'>{data[leng].projectsNav}</a>
-        <a className={n.navLink} href='#contact'>{data[leng].contactNav}</a>
+          <a className={n.navLink} href='#info'>Info</a>
+          <a className={n.navLink} href='#projects'>{data[leng].projectsNav}</a>
+          <a className={n.navLink} href='#contact'>{data[leng].contactNav}</a>
         </div>
       </div>
     </>
